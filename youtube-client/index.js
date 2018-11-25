2ad41016a2a415240c3eff4a021ffb70e14ed30a
Victoria Udodova <hentmire@gmail.com>
2018-11-25 23:20:24 +0300

    feat: add video loading and video slider

--- youtube-client/index.js
+++ youtube-client/index.js
@@ -1,72 +1,119 @@
-let request;
-function someFunc(e) {
+document.body.innerHTML = "<div class='search-form'><form><button type='submit' id='btn'></button><input type='search' placeholder='Video search' id='search'></form></div><section id='section'></section>";
+
+document.getElementById("btn").onclick = loadVideos;
+
+function loadVideos(e) {
 	e.preventDefault();
-    request = document.getElementById("search").value;
+    let request = document.getElementById("search").value;
 
-loadVideos().then( (data) => {
-    getVideoInfo(data, videoInfo);
-    getVideoStatistics(videoInfo)/*.then ( (data) => {
-        videoInfo.viewCount = data.items[0].statistics.viewCount;
-        videoInfo.dislikeCount = data.items[0].statistics.dislikeCount;
-        videoInfo.likeCount = data.items[0].statistics.likeCount;
-    });*/
-    let url = videoInfo.videoThumbnail[0];
-    console.log(videoInfo);
-    document.getElementById('first').innerHTML = `<img src=${url} alt = video-preview>`;
-});
+    getVideoData(request)
+    .then( data => {
+            let videoInfo = getVideoSnippets(data);
+            return videoInfo;
+        })
+        .then(videoInfo => {
+            getVideos(videoInfo);
+            return videoInfo;
+        })
+        .then( videoInfo => {
+            let videoNum = addSectionDiv();
+            addVideo(videoInfo, 0, videoNum);
+            addSlider(videoInfo, videoNum);
+        })
 }
-document.getElementById("btn").onclick = someFunc;
-let videoInfo = {};
 
-function loadVideos() {
+function getVideoData(request) {
     let url = `https://www.googleapis.com/youtube/v3/search?key=AIzaSyCTWC75i70moJLzyNh3tt4jzCljZcRkU8Y&type=video&maxResults=8&part=snippet&q=${request}`;
 	return fetch(url)
 		.then(response => response.json())
 }
 
-function getVideoInfo(data, objInfo) {
-    objInfo.videoThumbnail = [];
-    objInfo.publicationDate = [];
-    objInfo.videoTitle = [];
-    objInfo.videoDescription = [];
-    objInfo.videoAuthor = [];
-    objInfo.id = [];
-    objInfo.videoUrl = [];
-    objInfo.viewCount = [];
-    objInfo.dislikeCount = [];
-    objInfo.likeCount = [];
+function getVideoSnippets(data) {
+    let videoInfo = {
+        videoThumbnail: [],
+        publicationDate: [],
+        videoTitle: [],
+        videoDescription: [],
+        videoAuthor: [],
+        id: [],
+        videoUrl: [],
+        viewCount: [],
+        likeCount: []
+    };
     for(let i = 0; i < 8; i++) {
-        objInfo.videoThumbnail[i] = data.items[i].snippet.thumbnails.medium.url;
-        objInfo.publicationDate[i] = data.items[i].snippet.publishedAt;
-        objInfo.videoTitle[i] = data.items[i].snippet.title;
-        objInfo.videoDescription[i] = data.items[i].snippet.description;
-        objInfo.videoAuthor[i] = data.items[i].snippet.channelTitle;
-        objInfo.id[i] = data.items[i].id.videoId;
-        objInfo.videoUrl[i] = `https://www.youtube.com/watch?v=${objInfo.id[i]}`;
+        videoInfo.videoThumbnail[i] = data.items[i].snippet.thumbnails.medium.url;
+        videoInfo.publicationDate[i] = data.items[i].snippet.publishedAt;
+        videoInfo.videoTitle[i] = data.items[i].snippet.title;
+        videoInfo.videoDescription[i] = data.items[i].snippet.description;
+        videoInfo.videoAuthor[i] = data.items[i].snippet.channelTitle;
+        videoInfo.id[i] = data.items[i].id.videoId;
+        videoInfo.videoUrl[i] = `https://www.youtube.com/watch?v=${videoInfo.id[i]}`;
     }
-    /*objInfo.videoThumbnail = data.items[0].snippet.thumbnails.medium.url;
-    objInfo.publicationDate = data.items[0].snippet.publishedAt;
-    objInfo.videoTitle = data.items[0].snippet.title;
-    objInfo.videoDescription = data.items[0].snippet.description;
-    objInfo.videoAuthor = data.items[0].snippet.channelTitle;
-    objInfo.id = data.items[0].id.videoId;*/
+    return videoInfo;
 }
 
-function getVideoStatistics(objInfo) {
+function getVideos(videoInfo) {
+    let arr = [];
     for(let i = 0; i < 8; i++) {
-        let url = `https://www.googleapis.com/youtube/v3/videos?key=AIzaSyCTWC75i70moJLzyNh3tt4jzCljZcRkU8Y&part=statistics&id=${objInfo.id[i]}`;
-        fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            objInfo.viewCount[i] = data.items[0].statistics.viewCount;
-            objInfo.dislikeCount[i] = data.items[0].statistics.dislikeCount
-            objInfo.likeCount[i] = data.items[0].statistics.likeCount;
-            console.log(i);
-        })
+        arr[i] = getVideoStatistics(videoInfo, i);
     }
+    Promise.all(arr);
+}
 
-    /*let url = `https://www.googleapis.com/youtube/v3/videos?key=AIzaSyCTWC75i70moJLzyNh3tt4jzCljZcRkU8Y&part=statistics&id=${objInfo.id[i]}`;
+function getVideoStatistics(videoInfo, i) {
+    let url = `https://www.googleapis.com/youtube/v3/videos?key=AIzaSyCTWC75i70moJLzyNh3tt4jzCljZcRkU8Y&part=statistics&id=${videoInfo.id[i]}`;
     return fetch(url)
     .then(response => response.json())
-    .then(data => objInfo.data.push(data))*/
+    .then(data => {
+        videoInfo.viewCount[i] = data.items[0].statistics.viewCount;
+        videoInfo.likeCount[i] = data.items[0].statistics.likeCount;
+    })
+}
+
+function addSectionDiv() {
+    if (document.documentElement.clientWidth <= 1200 && document.documentElement.clientWidth > 700) {
+        document.getElementById("section").innerHTML = "<div class='video-load'><div class='video' id='video0'></div><div class='video' id='video1'></div></div><div class='video-slider' id='slider'></div>";
+        return 2;
+    } else if (document.documentElement.clientWidth <= 700) {
+        document.getElementById("section").innerHTML = "<div class='video-load'><div class='video' id='video0'></div></div><div class='video-slider' id='slider'></div>";
+        return 1;
+    } else {
+        document.getElementById("section").innerHTML = "<div class='video-load'><div class='video' id='video0'></div><div class='video' id='video1'></div><div class='video' id='video2'></div><div class='video' id='video3'></div></div><div class='video-slider' id='slider'></div>";
+        return 4;
+    }
+
+}
+
+function addVideo(videoInfo, start, end) {
+    for(let i = start; i < end; i++) {
+        if (videoInfo.videoDescription[i] == '') {videoInfo.videoDescription[i] = "No video description"};
+        let data = videoInfo.publicationDate[i].substr(0, 10);
+        videoList = document.getElementsByClassName('video');
+        videoList[(i - start)].innerHTML =  `<figure><img src=${videoInfo.videoThumbnail[i]} alt = video-preview><figcaption><a href=${videoInfo.videoUrl[i]}>${videoInfo.videoTitle[i]}</a></figcaption></figure>
+        <table class="video-info"><tr><td><i class="fa fa-male"></i></td><td>${videoInfo.videoAuthor[i]}</td></tr>
+        <tr><td><i class="fa fa-calendar"></i></td><td>${data}</td></tr>
+        <tr><td><i class="fa fa-eye"></i></td><td>${videoInfo.viewCount[i]}</td></tr>
+        <tr><td><i class="fa fa-thumbs-up"></i></td><td>${videoInfo.likeCount[i]}</td></tr>
+        </table><div class="video-description"><p>${videoInfo.videoDescription[i]}</p></div>`;
+    }
+}
+
+function addSlider(videoInfo, videoNum) {
+    if (videoNum === 4) {
+        document.getElementById("slider").innerHTML = "<div class='video-info-nav'><a class='current video-nav' href='#' data-slide='0'>1</a><a class='video-nav' href='#' data-slide='1'>2</a></div>";
+    } else if (videoNum == 2) {
+        document.getElementById("slider").innerHTML = "<div class='video-info-nav'><a class='current video-nav' href='#' data-slide='0'>1</a><a class='video-nav' href='#' data-slide='1'>2</a><a class='video-nav' href='#' data-slide='2'>3</a><a class='video-nav' href='#' data-slide='3'>4</a></div>";
+    } else {
+        document.getElementById("slider").innerHTML = "<div class='video-info-nav'><a class='current video-nav' href='#' data-slide='0'>1</a><a class='video-nav' href='#' data-slide='1'>2</a><a class='video-nav' href='#' data-slide='2'>3</a><a class='video-nav' href='#' data-slide='3'>4</a><a class='video-nav' href='#' data-slide='4'>5</a><a class='video-nav' href='#' data-slide='5'>6</a><a class='video-nav' href='#' data-slide='6'>7</a><a class='video-nav' href='#' data-slide='7'>8</a></div>";
+    }
+
+    let videoSlides = document.getElementsByClassName('video-nav');;
+    for (let i = 0; i < videoSlides.length; i++) {
+        let n = i*videoNum;
+        videoSlides[i].addEventListener('click', e => {
+            addVideo(videoInfo, n, (n+videoNum));
+            [].forEach.call(videoSlides, item => item.classList.remove('current'));
+            videoSlides[i].classList.add('current');
+        });
+    }
 }
