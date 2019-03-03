1d17fb8b06134356cb8cd0f4fc12956c1a053f65
Victoria Udodova <hentmire@gmail.com>
2019-03-03 14:48:49 +0300

    refact: use Event bubbling, add keycode constants, change attribute property to class

--- codejam-dom/notifications/notification.js
+++ codejam-dom/notifications/notification.js
@@ -1,8 +1,8 @@
-if (!localStorage.getItem('checkbox')) {
+if (!localStorage.getItem('notificationIsDisabled')) {
 	let notification = document.getElementById('notification');
 	let checkbox = document.getElementById('disable-messages');
 	let closingButton = document.getElementById('icon-close');
-	let notificationMessage = [
+	let notificationMessages = [
 	[['Message of the day'],['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales elit eget congue elementum. Praesent gravida finibus orci, et lacinia quam suscipit eget.']],
 	[['Message of the day'],['Vestibulum blandit eu lectus id aliquet. Etiam eu velit aliquet, convallis velit et, scelerisque velit. Sed urna leo, fermentum nec mollis nec, convallis et ex.']],
 	[['Message of the day'],['Curabitur fermentum dapibus purus, ac pretium tortor elementum a. Nulla nec lorem libero. Mauris eleifend risus vitae leo hendrerit iaculis.']],
@@ -11,42 +11,49 @@ if (!localStorage.getItem('checkbox')) {
 	[['Message of the day'],['Curabitur erat lectus, hendrerit in faucibus id, sagittis eget dolor. Cras a arcu et odio facilisis dignissim quis ut diam. ']]
 	];
 	let message = document.getElementById('info-slides');
+	let slider = document.getElementById('slides-nav');
 	let links = document.getElementsByClassName('slide-nav');
 	let prevButton = document.getElementById('prev');
 	let nextButton = document.getElementById('next');
+	let rightArrowKeyCode = 39;
+	let leftArrowKeyCode = 37;
+	let escapeKeyCode = 27;
 
-	setTimeout(openNotification, 5000); //загрузка нотификации через 5 секунд
+	window.addEventListener('load', openNotificationAfterFiveSeconds);	
+	function openNotificationAfterFiveSeconds () {//notification open in 1 seconds
+		setTimeout(openNotification, 1000);
+	}
 	function openNotification() {
-		notification.removeAttribute('hidden');
+		notification.classList.remove('hidden');
 	}
 
-	function setMessage(num) { //загрузка текстового message
-		message.firstChild.innerHTML = notificationMessage[num][0];
-		message.lastChild.innerHTML = notificationMessage[num][1];
+	function setMessage(num) { //text message download
+		message.firstChild.innerHTML = notificationMessages[num][0];
+		message.lastChild.innerHTML = notificationMessages[num][1];
 	}
 	setMessage(0);
 
-	closingButton.addEventListener('click', closeNotification); //при нажатии на кнопку закрытия
+	closingButton.addEventListener('click', closeNotification); //when press the close button
 	function closeNotification() {
-		notification.setAttribute('hidden', 'true');
+		notification.classList.add('hidden');
 		if (checkbox.checked === true) {
-			localStorage.setItem('checkbox', 'checked');
+			localStorage.setItem('notificationIsDisabled', 'true');
 		}
 	}
-	
-	for (let i = 0; i < links.length; i++) { //слайдер
-		links[i].addEventListener('click', e => showMessage(i));
-	}
-	function showMessage(num) {
+
+	slider.addEventListener('click', showMessage);
+	function showMessage(e) {
+		let target = e.target;
 		[].forEach.call(links, item => item.classList.remove('current'));
-		links[num].classList.add('current');
+		target.classList.add('current');
+		num = target.getAttribute('data-slide');
 		setMessage(num);
 	}
 
-	nextButton.addEventListener('click', e => toSlide('next'));//перемещение по стрелкам
+	nextButton.addEventListener('click', e => toSlide('next'));//slider arrows movement
 	prevButton.addEventListener('click', e => toSlide('prev'));
 	function toSlide(side) {
-		let currentSlideNum = document.querySelector('.info-slides-nav a.current').getAttribute("data-slide");
+		let currentSlideNum = document.querySelector('.current').getAttribute('data-slide');
 		let nextSlideNum = null;
 		if (side === 'next') {
 			+currentSlideNum === 5 ? nextSlideNum = 0 : nextSlideNum = +currentSlideNum + 1;
@@ -58,16 +65,16 @@ if (!localStorage.getItem('checkbox')) {
 		setMessage(nextSlideNum);
 	}
 
-	addEventListener('keydown', toSlideKey); //действия с клавиатуры
+	addEventListener('keydown', toSlideKey); //keyboard action
 	function toSlideKey() {
 		switch(event.keyCode) {
-	  		case 39:
+	  		case rightArrowKeyCode:
 	  			toSlide('next');
 	  			break;
-	  		case 37:
+	  		case leftArrowKeyCode:
 	  			toSlide('prev');
 	  			break;
-	  		case 27:
+	  		case escapeKeyCode:
 	  			closeNotification();
 	  			break;
 	  	}
